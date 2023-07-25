676102ed5bf87b18f229812d349ae21776af8109
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-07-25 21:47:59 +1000

    Added News Layout file

---
+++ src/components/NewsLayout1.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsLayout1 extends Component {
+  render() {
+    let { title, description, urlToImage, news} = this.props;
+    return (
+        <div className="col-md-3">
+            <div className="card" style={{ width: "18rem" }}>
+                <img src={urlToImage} className="card-img-top" alt="..."/>
+                <p class="tag">{news}</p>
+                <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                </div>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default NewsLayout1
