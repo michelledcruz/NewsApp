4e6b1dc7a177f7051c7bc8b746b60bc7c0742229
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-07-28 11:41:41 +1000

    Updated the news tag and css

--- src/components/NewsLayout1.js
+++ src/components/NewsLayout1.js
@@ -7,7 +7,7 @@ export class NewsLayout1 extends Component {
         <div className="col-md-3">
             <div className="card" style={{ width: "18rem" }}>
                 <img src={urlToImage} className="card-img-top" alt="..."/>
-                <p class="tag">{news}</p>
+                <p class="tag" style={{ backgroundColor: "#a71a1a", color: "white", padding: "1px 10px", width: "180px", top: "-26px", position: "relative", font: "14px" }}>{news}</p>
                 <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
