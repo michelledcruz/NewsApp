e203c3982de87ee5459c147f833a8c516c84cab6
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-08-15 21:51:58 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <NavBar/>
-        <News category={'general'} pageSize={6}/>
-        <News category={'health'} pageSize={3}/>
-        <News category={'entertainment'} pageSize={6}/>
+        <Switch>
+          <Route path="/general">
+            <News category={'general'} pageSize={6}/>
+          </Route>
+          <Route path="/health">
+            <News category={'health'} pageSize={6}/>
+          </Route>
+          <Route path="/entertainment">
+            <News category={'entertainment'} pageSize={6}/>
+          </Route>
+        </Switch>
       </div>
+      </Router>
     )
   }
 }
+
