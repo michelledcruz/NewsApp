d57c7e2da32c5bff3382c11b6b35415439e2e05b
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-08-28 16:33:11 +1000

    Added react router on the application

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import { Routes, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
+      <>
       <div>
         <NavBar/>
-        <News category={'general'} pageSize={6}/>
-        <News category={'health'} pageSize={3}/>
-        <News category={'entertainment'} pageSize={6}/>
+        <Routes>
+          <Route path="/" element={<News category={'general'} pageSize={6}/>} />
+          <Route path="/health" element={<News category={'health'} pageSize={6}/>} />
+          <Route path="/entertainment" element={<News category={'entertainment'} pageSize={6}/>} />
+          <Route path="/general" element={<News category={'general'} pageSize={6}/>} />
+          <Route path="/us" element={<h1>heyyy</h1>} />
+        </Routes>
       </div>
+      </>
     )
   }
 }
