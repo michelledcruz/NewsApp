d650f6d32dccbd7eb7d10a6e7e22ec8a416d1cef
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-08-28 10:14:16 +1000

    Reverted react router changes App.js

--- src/App.js
+++ src/App.js
@@ -3,32 +3,16 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
       <div>
         <NavBar/>
-        <Switch>
-          <Route path="/general">
-            <News category={'general'} pageSize={6}/>
-          </Route>
-          <Route path="/health">
-            <News category={'health'} pageSize={6}/>
-          </Route>
-          <Route path="/entertainment">
-            <News category={'entertainment'} pageSize={6}/>
-          </Route>
-        </Switch>
+        <News category={'general'} pageSize={6}/>
+        <News category={'health'} pageSize={3}/>
+        <News category={'entertainment'} pageSize={6}/>
       </div>
-      </Router>
     )
   }
 }
-
