dc67cb2d5de53e189dffdfdfccd13468f9747954
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-09-05 19:45:18 +1000

    Update NewsItem.js
    Added news author and source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, urlToImage } = this.props;
+    let { title, description, urlToImage, author, source} = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
             <img src={urlToImage} className="card-img-top" alt="..."/>
+           
             <div className="card-body">
+            <span class="badge bg-danger">{source}</span>
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            {author
+              ? <p class="card-text">By {author}  </p>
+              : ""
+            } 
             </div>
         </div>
       </div>
