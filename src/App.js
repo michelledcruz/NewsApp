438feb31ada30b10aeef445f4b95370e823ef0d2
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-09-12 22:24:02 +1000

    Update App.js
    Imported font file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './Font.css';
 
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import { Routes, Route } from 'react-router-dom';
+
 
 export default class App extends Component {
   render() {
@@ -12,13 +14,13 @@ export default class App extends Component {
       <div>
         <NavBar/>
         <Routes>
-          <Route exact path="/" element={<News key={"general"} category={'general'} pageSize={6}/>} />
-          <Route exact path="/health" element={<News key={"health"} category={'health'} pageSize={6}/>} />
-          <Route exact path="/entertainment" element={<News key={"entertainment"} category={'entertainment'} pageSize={6}/>} />
-          <Route exact path="/general" element={<News key={"general"} category={'general'} pageSize={6}/>} />
-          <Route exact path="/business" element={<News key={"business"} category={'business'} pageSize={6}/>} />
-          <Route exact path="/science" element={<News key={"science"} category={'science'} pageSize={6}/>} />
-          <Route exact path="/technology" element={<News key={"technology"} category={'technology'} pageSize={6}/>} />
+          <Route exact path="/" element={<News key={"general"} category={'general'} pageSize={10}/>} />
+          <Route exact path="/health" element={<News key={"health"} category={'health'} pageSize={10}/>} />
+          <Route exact path="/entertainment" element={<News key={"entertainment"} category={'entertainment'} pageSize={10}/>} />
+          <Route exact path="/general" element={<News key={"general"} category={'general'} pageSize={10}/>} />
+          <Route exact path="/business" element={<News key={"business"} category={'business'} pageSize={10}/>} />
+          <Route exact path="/science" element={<News key={"science"} category={'science'} pageSize={10}/>} />
+          <Route exact path="/technology" element={<News key={"technology"} category={'technology'} pageSize={10}/>} />
           </Routes>
       </div>
       </>
